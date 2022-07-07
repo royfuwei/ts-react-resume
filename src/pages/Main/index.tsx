1fb3af73aba0263a920f739e1f65edc77a6eeb0c
royfuwei <royfuwei@gmail.com>
2022-07-07 17:46:50 +0800

    feat: create main page v1

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,47 @@
+import { Avatar, Container, Grid } from '@mui/material';
+import avatar from '../../assets/images/avatar.jpg';
+
 import './MainPage.scss';
 
 export const MainPage = () => {
     return (
         <div className="main-page">
-            Main Page
+            <Container>
+                <Grid container spacing={{ xs: 1, md: 2}}>
+                    <Grid item md={6} xs={12} order={{xs: 2, md: 1}}>
+                        <div className='main-avatar-side'>
+                            <Avatar alt="Roy Chuang" sx={{
+                                width: {
+                                    xs: '80vw',
+                                    md: '30vw'
+                                },
+                                height: {
+                                    xs: '80vw',
+                                    md: '30vw'
+                                },
+                                fontSize: 'calc(36px + 5vmin);'
+                            }} src={avatar} />
+                        </div>
+                    </Grid>
+
+                    <Grid item md={6} xs={12} order={{xs: 1, md: 2}}>
+                        <div className="main-info-side">
+                            <div className="main-info-title">
+                                Hello
+                            </div>
+                            <div className="main-info-subSide">
+                                <div className="main-info-subTitle">
+                                    關於我
+                                </div>
+                                <div className="main-info-detail">
+                                    大家好...
+                                </div>
+                            </div>
+                            <br />
+                        </div>
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     );
 };
