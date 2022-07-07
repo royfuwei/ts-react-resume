847c1083703e5bb6a227913f93b6996b9d44d88b
royfuwei <royfuwei@gmail.com>
2022-07-07 19:35:43 +0800

    feat: update footer layout

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,19 +1,44 @@
 import './Footer.scss';
 import moment from 'moment';
-import { Container } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 
 export const FooterLayout = () => {
   const year = moment().format('Y');
   return (
     <footer>
-      <Container>
+      <Container sx={{ minWidth: '90vw', marginTop: { xs: '50px', md: '0'}}}>
         {/* Copyright © { year } royfuwei. */}
         <div className='footer'>
-          <div className='footer-line'></div>
+          {/* <div className='footer-line'></div>
           <div className='footer-side'>
-            Copyright © { year } royfuwei.
+            © { year } By royfuwei.
+          </div> */}
+          <div className='footer-line'></div>
+          <div className="footer-side">
+            <Grid container spacing={{ md: 3, xs: 1}}>
+              <Grid item md={4} xs={12}>
+                <div className="footer-side-item">
+                  <div className="title">
+                    Email
+                  </div>
+                  <div className="value">
+                    <a href="mailto:royfuwei@gmail.com">royfuwei@gmail.com</a>
+                  </div>
+                </div>
+              </Grid>
+              <Grid item md={4} xs={12}>
+                <div className="footer-side-item">
+                </div>
+              </Grid>
+              <Grid item md={4} xs={12}>
+                <div className='footer-side-item footer-copyright'>
+                  © { year } By royfuwei.
+                </div>
+              </Grid>
+            </Grid>
           </div>
         </div>
+        
       </Container>
     </footer>
   );
