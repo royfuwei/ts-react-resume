002f13ca2494c9eb2323f14cfdb3219690147473
royfuwei <royfuwei@gmail.com>
2022-07-13 09:04:51 +0800

    chore: 調整HoverRating config

--- src/component/HoverRating/index.tsx
+++ src/component/HoverRating/index.tsx
@@ -1,18 +1,30 @@
 import { Box, Rating } from "@mui/material";
 import React from "react";
 import StarIcon from '@mui/icons-material/Star';
+import './HoverRating.scss';
 
 const labels: { [index: string]: string } = {
-  0.5: 'Useless',
-  1: 'Useless+',
-  1.5: 'Poor',
-  2: 'Poor+',
-  2.5: 'Ok',
-  3: 'Ok+',
-  3.5: 'Good',
-  4: 'Good+',
-  4.5: 'Excellent',
-  5: 'Excellent+',
+  0.5: '有研究沒用過',
+  1: '有研究測試過',
+  1.5: '很少使用',
+  2: '熟練度不高',
+  2.5: '熟練度一般',
+  3: '熟練度一般，工作使用',
+  3.5: '熟練度中上，工作偶爾使用',
+  4: '熟練度中上，工作長使用',
+  4.5: '熟練度高',
+  5: '熟練度高+',
+
+  // 0.5: 'Useless',
+  // 1: 'Useless+',
+  // 1.5: 'Poor',
+  // 2: 'Poor+',
+  // 2.5: 'Ok',
+  // 3: 'Ok+',
+  // 3.5: 'Good',
+  // 4: 'Good+',
+  // 4.5: 'Excellent',
+  // 5: 'Excellent+',
 };
 
 function getLabelText(value: number) {
@@ -25,6 +37,7 @@ export function HoverRating({ rating = 3.5 }: { rating?: number}) {
 
   return (
     <Box
+      className="HoverRating"
       sx={{
         width: 200,
         display: 'flex',
@@ -45,7 +58,7 @@ export function HoverRating({ rating = 3.5 }: { rating?: number}) {
         emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
       />
       {value !== null && (
-        <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+        <Box className="label" sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
       )}
     </Box>
   );
