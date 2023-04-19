4b23c99d4ea3f1ec7e971fae19c4650cb692d185
royfuwei <royfuwei@gmail.com>
2023-04-20 01:46:36 +0800

    [feat] App router 改用 HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from './layout/Main';
 import TestPage from './pages/Test';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <Routes>
         <Route path='*' element={<MainLayout/>}/>
         <Route key='test' path='/test' element={<TestPage/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
